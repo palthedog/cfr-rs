7274c438a5ba578145043a2f0a143d504380ce67
Naoki Iwasaki <niwasaki@f13g.com>
2023-03-06 16:42:23 +0900

    remove fragmented codes

--- kuhn/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Initialize env_logger with a default log level of INFO.
-    env_logger::init_from_env(
-        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
-    );
-
-    let mut trainer = kuhn::Trainer::new();
-    trainer.train(10_000_000);
-}
